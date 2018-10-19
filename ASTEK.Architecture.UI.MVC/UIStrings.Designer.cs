9da34e388f7349a472b550284ec3897174fcc423
trabemananjara <trabemananjara@MAU-DT110>
2018-10-19 09:01:40 +0400

    Page title

--- ASTEK.Architecture.UI.MVC/UIStrings.Designer.cs
+++ ASTEK.Architecture.UI.MVC/UIStrings.Designer.cs
@@ -19,7 +19,7 @@ namespace ASTEK.Architecture.UI.MVC {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "15.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     public class UIStrings {
@@ -57,6 +57,15 @@ namespace ASTEK.Architecture.UI.MVC {
             }
             set {
                 resourceCulture = value;
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Application.
+        /// </summary>
+        public static string App_Title {
+            get {
+                return ResourceManager.GetString("App_Title", resourceCulture);
             }
         }
         
