ac9e5420c79e9ec996681ffacd59a8b2baea9d66
trabemananjara <trabemananjara@MAU-DT110>
2018-10-23 10:56:54 +0400

    Toogle Subscription ajax & partialViewResult

---
+++ ASTEK.Architecture.UI.MVC/Scripts/common-js/subscribe.js
@@ -0,0 +1,22 @@
+﻿function toogleSubscription(subscribed) {
+    $.ajax({
+        data: 'accountId='+subscribed,
+        url: 'Subscribe/ToogleSubscription',
+        success: function (response) {
+            applyResult(response);
+        }
+    });
+}
+
+function applyResult(response) {
+    var result = JSON.parse(response);
+
+    if (result.Success == 'true') {
+        if (result.IsSubscribed == 'true') {
+            $('#subscribeBtn').text('Se désabonner');
+        }
+        else {
+            $('#subscribeBtn').text('S\'abonner');
+        }
+    }
+}
